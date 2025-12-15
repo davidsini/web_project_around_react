91f77316bc19a4f84c25a49dfde09a5dcfcbbbd3
Dēivid Sans <david.mancillas@icloud.com>
2025-12-14 21:36:21 -0600

    correcciones y añadir EditAvatar y EditProfile

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import { useState } from "react";
-import Popup from "./Popup";
+import Popup from "./components/Popup/Popup.jsx";
+import NewCard from "../form/NewCard/NewCard.jsx";
+import EditProfile from "../form/EditProfile/EditProfile.jsx";
+import EditAvatar from "../form/EditAvatar/EditAvatar.jsx";
 
 export default function Main() {
   const [popup, setPopup] = useState(null);
 
-  const newCardPopup = { title: "Nuevo lugar", children: <NewCard /> };
+  const newCardPopup = {
+    title: "Nuevo lugar",
+    children: <NewCard />,
+  };
 
-  function handleOpenPopup(popup) {
-    setPopup(popup);
+  const editProfilePopup = {
+    title: "Editar perfil",
+    children: <EditProfile />,
+  };
+
+  const editAvatarPopup = {
+    title: "Cambiar foto de perfil",
+    children: <EditAvatar />,
+  };
+
+  function handleOpenPopup(popupToOpen) {
+    setPopup(popupToOpen);
   }
 
   const handleClosePopup = () => {
@@ -25,20 +41,26 @@ export default function Main() {
               src="/public/images/avatar.png"
               alt="Imagen de perfil del usuario"
             />
-            <div className="profile__avatar-overlay"></div>
+
+            <div
+              className="profile__avatar-overlay"
+              onClick={() => handleOpenPopup(editAvatarPopup)}></div>
           </div>
           <div className="profile__info-w-button">
             <div className="profile__info">
               <h1 className="profile__name">Jacques Cousteau</h1>
+
               <img
                 src="/public/images/edit-button.svg"
                 className="profile__edit-button"
                 alt="botón de edición"
+                onClick={() => handleOpenPopup(editProfilePopup)}
               />
             </div>
             <p className="profile__occupation">Explorador</p>
           </div>
         </div>
+
         <button
           aria-label="Add card"
           className="profile__add-button"
