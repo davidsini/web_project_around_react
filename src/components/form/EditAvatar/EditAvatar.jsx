b2776a89f5dfe206acc6d5f697b98b23904ab45f
Dēivid Sans <david.mancillas@icloud.com>
2025-12-14 19:02:23 -0600

    crear carpetas edit

---
+++ src/components/form/EditAvatar/EditAvatar.jsx
@@ -0,0 +1,21 @@
+export default function EditAvatar() {
+  return (
+    <form className="popup__form" name="edit-avatar-form" noValidate>
+      <label className="popup__field">
+        <input
+          className="popup__input popup__input_type_url"
+          id="avatar-url-input"
+          type="url"
+          name="avatar"
+          placeholder="Enlace a la imagen de perfil"
+          required
+        />
+        <span className="popup__error" id="avatar-url-input-error"></span>
+      </label>
+
+      <button className="button popup__button" type="submit">
+        Guardar
+      </button>
+    </form>
+  );
+}
