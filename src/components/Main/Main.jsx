69d589ce8f9a2acae6475077b632079f2849a2e2
Dēivid Sans <david.mancillas@icloud.com>
2025-12-14 21:46:22 -0600

    paso 5 y correcciones css

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,6 +5,27 @@ import NewCard from "../form/NewCard/NewCard.jsx";
 import EditProfile from "../form/EditProfile/EditProfile.jsx";
 import EditAvatar from "../form/EditAvatar/EditAvatar.jsx";
 
+import Card from "./components/Card/Card.jsx";
+
+const cards = [
+  {
+    isLiked: false,
+    _id: "5d1f0611d321eb4bdcd707dd",
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+    owner: "5d1f0611d321eb4bdcd707dd",
+    createdAt: "2019-07-05T08:10:57.741Z",
+  },
+  {
+    isLiked: false,
+    _id: "5d1f064ed321eb4bdcd707de",
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+    owner: "5d1f0611d321eb4bdcd707dd",
+    createdAt: "2019-07-05T08:11:58.324Z",
+  },
+];
+
 export default function Main() {
   const [popup, setPopup] = useState(null);
 
@@ -32,43 +53,50 @@ export default function Main() {
   };
 
   return (
-    <main className="profile">
-      <div className="profile__main-container">
-        <div className="profile__data">
-          <div className="profile__avatar-wrapper">
-            <img
-              className="profile__picture"
-              src="/public/images/avatar.png"
-              alt="Imagen de perfil del usuario"
-            />
-
-            <div
-              className="profile__avatar-overlay"
-              onClick={() => handleOpenPopup(editAvatarPopup)}></div>
-          </div>
-          <div className="profile__info-w-button">
-            <div className="profile__info">
-              <h1 className="profile__name">Jacques Cousteau</h1>
-
+    <main className="content">
+      <section className="profile">
+        <div className="profile__main-container">
+          <div className="profile__data">
+            <div className="profile__avatar-wrapper">
               <img
-                src="/public/images/edit-button.svg"
-                className="profile__edit-button"
-                alt="botón de edición"
-                onClick={() => handleOpenPopup(editProfilePopup)}
+                className="profile__picture"
+                src="/public/images/avatar.png"
+                alt="Imagen de perfil del usuario"
               />
+              <div
+                className="profile__avatar-overlay"
+                onClick={() => handleOpenPopup(editAvatarPopup)}></div>
             </div>
-            <p className="profile__occupation">Explorador</p>
+            <div className="profile__info-w-button">
+              <div className="profile__info">
+                <h1 className="profile__name">Jacques Cousteau</h1>
+                <img
+                  src="/public/images/edit-button.svg"
+                  className="profile__edit-button"
+                  alt="botón de edición"
+                  onClick={() => handleOpenPopup(editProfilePopup)}
+                />
+              </div>
+              <p className="profile__occupation">Explorador</p>
+            </div>
           </div>
+
+          <button
+            aria-label="Add card"
+            className="profile__add-button"
+            type="button"
+            onClick={() => handleOpenPopup(newCardPopup)}
+          />
         </div>
+      </section>
 
-        <button
-          aria-label="Add card"
-          className="profile__add-button"
-          type="button"
-          onClick={() => handleOpenPopup(newCardPopup)}
-        />
-      </div>
-
+      <section className="cards">
+        <ul className="cards__list">
+          {cards.map((card) => (
+            <Card key={card._id} card={card} />
+          ))}
+        </ul>
+      </section>
       {popup && (
         <Popup onClose={handleClosePopup} title={popup.title}>
           {popup.children}
---
+++ src/components/Main/components/Popup/Card.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Card(props) {
+  const { name, link } = props.card;
+
+  return (
+    <li className="card">
+      <img className="card__image" src={link} alt={name} />
+      <button
+        aria-label="Delete card"
+        className="card__delete-button"
+        type="button"
+      />
+      <div className="card__description">
+        <h2 className="card__title">{name}</h2>
+        <button
+          aria-label="Like card"
+          type="button"
+          className="card__like-button"
+        />
+      </div>
+    </li>
+  );
+}
